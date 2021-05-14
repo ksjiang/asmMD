64b1bcfb4cf6082305051aa1c0d533aba235112b
Kyle Jiang <siyu.jiang81@gmail.com>
2021-05-14 01:34:53 -0400

    Added functions

--- asmMD.asm
+++ asmMD.asm
@@ -1255,6 +1255,480 @@ LJenergyDiff endp
 
 
 
+LJpressureEval proc nPart:DWORD, X_:DWORD, L:DWORD, Pxyz_:DWORD
+	sub esp, 90h						;memory corruption much?
+	movdqu [ebp - 10h], xmm0
+	movdqu [ebp - 20h], xmm1
+	movdqu [ebp - 30h], xmm2
+	mov dword ptr [ebp - 34h], ebx
+	mov dword ptr [ebp - 38h], ecx
+	mov dword ptr [ebp - 3ch], edx
+	mov dword ptr [ebp - 40h], esi
+	mov dword ptr [ebp - 44h], edi
+	
+	; start
+	mov eax, [LJisset]
+	test eax, eax
+	jnz LJpressureEval2					;params already initialized, go evaluate the forces
+	
+	; initialize
+	push dword ptr [L]
+	call LJinit
+	
+LJpressureEval2:
+	mov eax, nPart
+	shl eax, 2
+	invoke GlobalAlloc, 40h, eax
+	mov dword ptr [ebp - 50h], eax		;cellmap
+
+	; clear out pressure
+	mov edi, Pxyz_
+	xor eax, eax
+	mov ecx, 8							;total of 4 quadwords (8 dwords)
+	rep stosd							;clear
+
+	mov eax, dword ptr [LJn]
+	mov ebx, eax
+	imul eax, ebx
+	imul eax, ebx
+	shl eax, 2
+	invoke GlobalAlloc, 40h, eax
+	mov dword ptr [ebp - 54h], eax		;cells
+	
+	; sort particles into cells
+	mov dword ptr [ebp - 58h], 0
+LJpressureEval3:
+	; extract particle coordinates
+	mov esi, dword ptr [ebp - 58h]
+	shl esi, 5
+	add esi, X_
+	movsd xmm0, qword ptr [esi]
+	divsd xmm0, qword ptr [LJdn]
+	cvttsd2si eax, xmm0
+	mov dword ptr [ebp - 5ch], eax		;nx
+	movsd xmm0, qword ptr [esi + 8]
+	divsd xmm0, qword ptr [LJdn]
+	cvttsd2si eax, xmm0
+	mov dword ptr [ebp - 60h], eax		;ny
+	movsd xmm0, qword ptr [esi + 10h]
+	divsd xmm0, qword ptr [LJdn]
+	cvttsd2si eax, xmm0
+	mov dword ptr [ebp - 64h], eax		;nz
+	imul eax, dword ptr [LJn]
+	add eax, dword ptr [ebp - 60h]
+	imul eax, dword ptr [LJn]
+	add eax, dword ptr [ebp - 5ch]
+	mov dword ptr [ebp - 8ch], eax		;ecx clobber :(
+	mov esi, dword ptr [ebp - 54h]
+	lea esi, [esi + 4 * eax]			;traverse the singly-linked list
+LJpressureEval4:
+	mov eax, dword ptr [esi]
+	test eax, eax
+	jz LJpressureEval5
+	mov esi, eax
+	jmp LJpressureEval4
+LJpressureEval5:
+	; allocate. esi is now pointing to last node.
+	invoke GlobalAlloc, 40h, 8
+	mov dword ptr [esi], eax			;set next of previously last node
+	mov ebx, dword ptr [ebp - 58h]
+	mov dword ptr [eax + 4], ebx		;append i to cell list
+	mov edi, dword ptr [ebp - 50h]
+	mov ecx, dword ptr [ebp - 8ch]		;restore ecx!!
+	mov dword ptr [edi + 4 * ebx], ecx	;append cell index into cellmap
+	inc ebx
+	mov dword ptr [ebp - 58h], ebx
+	cmp ebx, nPart
+	jb LJpressureEval3
+	; close loop
+	
+	invoke GlobalAlloc, 40h, 32
+	mov dword ptr [ebp - 80h], eax
+	invoke GlobalAlloc, 40h, 32
+	mov dword ptr [ebp - 84h], eax
+	invoke GlobalAlloc, 40h, 32
+	mov dword ptr [ebp - 88h], eax
+	
+	; calculate interactions
+	; big outer loop
+	mov dword ptr [ebp - 58h], 0
+LJpressureEval6:
+	mov ebx, dword ptr [LJn]
+	mov esi, ebx
+	imul esi, ebx
+	mov ecx, dword ptr [ebp - 58h]
+	mov edi, dword ptr [ebp - 50h]
+	mov eax, dword ptr [edi + 4 * ecx]
+	mov ecx, eax
+	xor edx, edx
+	idiv ebx
+	mov dword ptr [ebp - 5ch], edx
+	mov eax, ecx						;restore
+	xor edx, edx
+	idiv ebx
+	xor edx, edx
+	idiv ebx
+	mov dword ptr [ebp - 60h], edx
+	mov eax, ecx
+	xor edx, edx
+	idiv esi
+	xor edx, edx
+	idiv ebx
+	mov dword ptr [ebp - 64h], edx
+	
+	; loop over x offsets
+	mov dword ptr [ebp - 68h], 0ffffffffh
+LJpressureEval7:
+
+	; loop over y offsets
+	mov dword ptr [ebp - 6ch], 0ffffffffh
+LJpressureEval8:
+
+	; loop over z offsets
+	mov dword ptr [ebp - 70h], 0ffffffffh
+LJpressureEval9:
+	mov ecx, 3
+	lea edi, [ebp - 7ch]
+	lea esi, [ebp - 64h]
+	rep movsd
+	mov eax, dword ptr [ebp - 68h]
+	add dword ptr [ebp - 74h], eax
+	mov eax, dword ptr [ebp - 6ch]
+	add dword ptr [ebp - 78h], eax
+	mov eax, dword ptr [ebp - 70h]
+	add dword ptr [ebp - 7ch], eax
+	
+	; loop over dimensions, form correction vector
+	mov ecx, 8							;clear out the correction vector!
+	xor eax, eax
+	mov edi, dword ptr [ebp - 80h]
+	rep stosd
+	mov ecx, 2
+	xor edx, edx
+	movsd xmm0, qword ptr [LJLint]
+	movsd xmm1, qword ptr [NUNITY]
+	mulsd xmm1, xmm0					;xmm1 = -Lint
+LJpressureEval10:
+	mov ebx, dword ptr [LJn]
+	mov eax, dword ptr [ebp - 7ch + 4 * ecx]
+	cmp eax, 0
+	jge LJpressureEval11
+	; make PBC correction
+	add dword ptr [ebp - 7ch + 4 * ecx], ebx
+	mov eax, dword ptr [ebp - 80h]
+	movsd qword ptr [eax + 8 * edx], xmm0
+	
+LJpressureEval11:
+	mov eax, dword ptr [ebp - 7ch + 4 * ecx]
+	cmp eax, ebx
+	jl LJpressureEval12
+	;make PBC correction
+	sub dword ptr [ebp - 7ch + 4 * ecx], ebx
+	mov eax, dword ptr [ebp - 80h]
+	movsd qword ptr [eax + 8 * edx], xmm1
+	
+LJpressureEval12:
+	inc edx
+	dec ecx
+	cmp ecx, 0
+	jge LJpressureEval10
+	
+	; loop over neighbor cell list
+	mov ebx, dword ptr [ebp - 7ch]
+	imul ebx, dword ptr [LJn]
+	add ebx, dword ptr [ebp - 78h]
+	imul ebx, dword ptr [LJn]
+	add ebx, dword ptr [ebp - 74h]
+	mov esi, [ebp - 54h]
+	lea edi, [esi + 4 * ebx]			;traverse the singly-linked list
+	jmp LJpressureEval14
+LJpressureEval13:
+	mov edi, eax
+	mov ecx, [edi + 4]					;ecx -> particle j
+	mov edx, [ebp - 58h]				;edx index of* particle i
+	cmp ecx, edx
+	jz LJpressureEval14					;continue if same particle
+	mov eax, dword ptr [ebp - 84h]
+	mov esi, ecx						;go to j
+	shl esi, 5
+	add esi, X_
+	movdqu xmm0, [esi]
+	movdqu [eax], xmm0
+	movdqu xmm0, [esi + 10h]
+	movdqu [eax + 10h], xmm0
+	mov esi, edx						;go to i
+	shl esi, 5
+	add esi, X_
+	movdqu xmm0, [esi]
+	movdqu xmm1, [eax]
+	subpd xmm1, xmm0
+	movdqu [eax], xmm1
+	movdqu xmm0, [esi + 10h]
+	movdqu xmm1, [eax + 10h]
+	subpd xmm1, xmm0
+	movdqu [eax + 10h], xmm1
+	mov esi, dword ptr [ebp - 80h]		;go to corr
+	movdqu xmm0, [esi]
+	movdqu xmm1, [eax]
+	subpd xmm1, xmm0
+	movdqu [eax], xmm1
+	movdqu xmm0, [esi + 10h]
+	movdqu xmm1, [eax + 10h]
+	subpd xmm1, xmm0
+	movdqu [eax + 10h], xmm1
+	push [ebp - 88h]
+	lea eax, [LJrc]
+	push eax
+	push dword ptr [ebp - 84h]
+	call LJc
+
+	; get x and y components of stress tensor
+	mov eax, dword ptr [ebp - 88h]
+	movdqu xmm0, [eax]
+	mov ecx, dword ptr [ebp - 84h]
+	movdqu xmm1, [ecx]
+	mulpd xmm0, xmm1
+	mov edx, dword ptr [Pxyz_]
+	movdqu xmm1, [edx]
+	addpd xmm0, xmm1
+	movdqu [edx], xmm0
+
+	; get z (and extra) component of stress tensor
+	movdqu xmm0, [eax + 10h]
+	movdqu xmm1, [ecx + 10h]
+	mulpd xmm0, xmm1
+	movdqu xmm1, [edx + 10h]
+	addpd xmm0, xmm1
+	movdqu [edx + 10h], xmm0
+
+LJpressureEval14:
+	mov eax, dword ptr [edi]
+	test eax, eax
+	jnz LJpressureEval13					;fallthrough if all done
+	
+LJpressureEval15:
+	inc dword ptr [ebp - 70h]
+	cmp dword ptr [ebp - 70h], 1
+	jle LJpressureEval9
+	; close loop
+	inc dword ptr [ebp - 6ch]
+	cmp dword ptr [ebp - 6ch], 1
+	jle LJpressureEval8
+	; close loop
+	inc dword ptr [ebp - 68h]
+	cmp dword ptr [ebp - 68h], 1
+	jle LJpressureEval7
+	; close loop
+	
+	mov eax, dword ptr [ebp - 58h]
+	inc eax
+	mov dword ptr [ebp - 58h], eax
+	cmp eax, nPart
+	jb LJpressureEval6
+	; close loop
+	
+	; clean up and exit
+	invoke GlobalFree, dword ptr [ebp - 50h]
+	invoke GlobalFree, dword ptr [ebp - 80h]
+	invoke GlobalFree, dword ptr [ebp - 84h]
+	invoke GlobalFree, dword ptr [ebp - 88h]
+	
+	; need to free all of the elements of the linked list
+	mov dword ptr [ebp - 58h], 0
+LJpressureEval16:
+	mov ebx, dword ptr [ebp - 58h]
+	mov esi, dword ptr [ebp - 54h]
+	lea edi, [esi + 4 * ebx]
+	mov esi, dword ptr [edi]
+	jmp LJpressureEval18
+LJpressureEval17:
+	mov edi, esi
+	mov esi, dword ptr [edi]
+	invoke GlobalFree, edi
+LJpressureEval18:
+	test esi, esi
+	jnz LJpressureEval17					;fallthrough if all done
+	
+LJpressureEval19:
+	inc ebx
+	mov dword ptr [ebp - 58h], ebx
+	mov eax, dword ptr [LJn]
+	imul eax, eax
+	imul eax, dword ptr [LJn]
+	cmp ebx, eax
+	jb LJpressureEval16
+	
+	; now, free the cell list itself
+	invoke GlobalFree, dword ptr [ebp - 54h]
+
+	; correct for overcounting in stress tensor
+	mov ebx, dword ptr [Pxyz_]
+	movdqu xmm0, [ebx]
+	mov eax, 2
+	cvtsi2sd xmm1, eax
+	movlhps xmm1, xmm1
+	divpd xmm0, xmm1
+	movdqu [ebx], xmm0
+
+	movdqu xmm0, [ebx + 10h]
+	divpd xmm0, xmm1
+	movdqu [ebx + 10h], xmm0
+	
+	; restore and return
+	movdqu xmm0, [ebp - 10h]
+	movdqu xmm1, [ebp - 20h]
+	movdqu xmm2, [ebp - 30h]
+	mov ebx, dword ptr [ebp - 34h]
+	mov ecx, dword ptr [ebp - 38h]
+	mov edx, dword ptr [ebp - 3ch]
+	mov esi, dword ptr [ebp - 40h]
+	mov edi, dword ptr [ebp - 44h]
+	add esp, 90h
+	ret
+LJpressureEval endp
+
+
+
+LJstep proc nPart:DWORD, X_:DWORD, V_:DWORD, F_:DWORD, delT:DWORD
+	; enter
+	sub esp, 30h
+	movdqu [ebp - 10h], xmm0
+	mov dword ptr [ebp - 14h], ecx
+	mov dword ptr [ebp - 18h], esi
+
+	; copy velocities
+	mov ecx, dword ptr [nPart]
+	shl ecx, 5
+	invoke GlobalAlloc, 40h, ecx
+	mov dword ptr [ebp - 20h], eax		;ebp - 20h: velocity copy
+	push dword ptr [nPart]
+	push dword ptr [V_]
+	push dword ptr [ebp - 20h]
+	call dblBufferVec4Cpy
+
+	; copy forces
+	mov ecx, dword ptr [nPart]
+	shl ecx, 5
+	invoke GlobalAlloc, 40h, ecx
+	mov dword ptr [ebp - 24h], eax		;ebp - 24h: force copy
+	push dword ptr [nPart]
+	push dword ptr [F_]
+	push dword ptr [ebp - 24h]
+	call dblBufferVec4Cpy
+
+	; form constant vectors
+	invoke GlobalAlloc, 40h, 32h
+	mov dword ptr [ebp - 28h], eax		;ebp - 28h: dt vector
+	mov esi, dword ptr [delT]
+	movsd xmm0, qword ptr [esi]
+	movsd qword ptr [eax], xmm0
+	movsd qword ptr [eax + 8], xmm0
+	movsd qword ptr [eax + 10h], xmm0
+
+	invoke GlobalAlloc, 40h, 32h
+	mov dword ptr [ebp - 2ch], eax		;ebp - 2ch: 0.5 * dt2 vector
+	mov esi, dword ptr [delT]
+	movsd xmm0, qword ptr [esi]
+	mulsd xmm0, xmm0
+	mulsd xmm0, qword ptr [HALF]
+	movsd qword ptr [eax], xmm0
+	movsd qword ptr [eax + 8], xmm0
+	movsd qword ptr [eax + 10h], xmm0
+
+	; update positions
+	push dword ptr [nPart]
+	push dword ptr [ebp - 28h]
+	push dword ptr [ebp - 20h]
+	call dblBufferVec4Mul
+
+	push dword ptr [nPart]
+	push dword ptr [ebp - 2ch]
+	push dword ptr [ebp - 24h]
+	call dblBufferVec4Mul
+
+	push dword ptr [X_]
+	push dword ptr [nPart]
+	push dword ptr [ebp - 20h]
+	push dword ptr [X_]
+	call dblBuffer2Vec4Sum
+
+	push dword ptr [X_]
+	push dword ptr [nPart]
+	push dword ptr [ebp - 24h]
+	push dword ptr [X_]
+	call dblBuffer2Vec4Sum
+
+	; correct for 3D PBC
+	push dword ptr [LJLint]
+	push dword ptr [X_]
+	push dword ptr [nPart]
+	call pbc3d
+
+	; update velocities
+	push dword ptr [nPart]
+	push dword ptr [F_]
+	push dword ptr [ebp - 24h]
+	call dblBufferVec4Cpy
+
+	mov esi, dword ptr [delT]
+	movsd xmm0, qword ptr [esi]
+	mulsd xmm0, qword ptr [HALF]
+	mov eax, dword ptr [ebp - 2ch]		;ebp - 5ch: 0.5 * dt vector
+	movsd qword ptr [eax], xmm0
+	movsd qword ptr [eax + 8], xmm0
+	movsd qword ptr [eax + 10h], xmm0
+
+	push dword ptr [nPart]
+	push dword ptr [ebp - 2ch]
+	push dword ptr [ebp - 24h]
+	call dblBufferVec4Mul
+
+	push dword ptr [V_]
+	push dword ptr [nPart]
+	push dword ptr [ebp - 24h]
+	push dword ptr [V_]
+	call dblBuffer2Vec4Sum
+
+	; update forces
+	push dword ptr [F_]
+	push dword ptr [LJLint]
+	push dword ptr [X_]
+	push dword ptr [nPart]
+	call LJforceEval
+
+	; update velocities again (with new forces)
+	push dword ptr [nPart]
+	push dword ptr [F_]
+	push dword ptr [ebp - 24h]
+	call dblBufferVec4Cpy
+
+	push dword ptr [nPart]
+	push dword ptr [ebp - 2ch]
+	push dword ptr [ebp - 24h]
+	call dblBufferVec4Mul
+
+	push dword ptr [V_]
+	push dword ptr [nPart]
+	push dword ptr [ebp - 24h]
+	push dword ptr [V_]
+	call dblBuffer2Vec4Sum
+
+	; don't forget to free
+	invoke GlobalFree, dword ptr [ebp - 20h]
+	invoke GlobalFree, dword ptr [ebp - 24h]
+	invoke GlobalFree, dword ptr [ebp - 28h]
+	invoke GlobalFree, dword ptr [ebp - 2ch]
+
+	;exit
+	movdqu xmm0, [ebp - 10h]
+	mov ecx, dword ptr [ebp - 14h]
+	mov esi, dword ptr [ebp - 18h]
+	add esp, 30h
+	ret
+LJstep endp
+
+
+
 init3DGrid proc nPart:DWORD, dens:DWORD, L:DWORD, grid:DWORD
 	sub esp, 90h
 	movdqu [ebp - 10h], xmm0
@@ -1375,6 +1849,368 @@ init3DGrid endp
 
 
 
+pbc3d proc nPart:DWORD, X_:DWORD, L:DWORD
+	; enter
+	sub esp, 40h
+	movdqu [ebp - 10h], xmm0
+	movdqu [ebp - 20h], xmm1
+	mov dword ptr [ebp - 24h], ecx
+	mov dword ptr [ebp - 28h], ebx
+	mov dword ptr [ebp - 2ch], esi
+	mov dword ptr [ebp - 30h], edx
+	mov dword ptr [ebp - 34h], edi
+	stmxcsr dword ptr [ebp - 38h]		;save the mxcsr state
+
+	; change rounding mode to towards negative infinity
+	mov dword ptr [ebp - 3ch], 3f80h
+	ldmxcsr dword ptr [ebp - 3ch]
+
+	mov eax, [LJisset]
+	test eax, eax
+	jnz pbc3d1							;params already initialized
+	
+	; initialize
+	push dword ptr [L]
+	call LJinit
+
+COMMENT @
+pbc3d1:
+	mov esi, dword ptr [X_]
+	mov ecx, dword ptr [nPart]
+	xorpd xmm1, xmm1
+	xor ebx, ebx
+
+pbc3d2:
+	xor edx, edx
+
+pbc3d3:
+	lea edi, [ebx + 8 * edx]
+	movsd xmm0, qword ptr [esi + edi]
+	jmp pbc3d5
+
+pbc3d4:
+	subsd xmm0, qword ptr [LJLint]
+
+pbc3d5:
+	ucomisd xmm0, qword ptr [LJLint]
+	jae pbc3d4
+
+	jmp pbc3d7
+
+pbc3d6:
+	addsd xmm0, qword ptr [LJLint]
+
+pbc3d7:
+	ucomisd xmm0, xmm1
+	jb pbc3d6
+
+	movsd qword ptr [esi + edi], xmm0
+	inc edx
+	cmp edx, 3
+	jne pbc3d3
+
+	add ebx, 20h
+	loop pbc3d2
+@
+
+pbc3d1:
+	mov esi, dword ptr [X_]
+	mov ecx, dword ptr [nPart]
+	xor ebx, ebx
+
+pbc3d2:
+	xor edx, edx
+
+pbc3d3:
+	lea edi, [ebx + 8 * edx]
+	movsd xmm0, qword ptr [esi + edi]
+	ucomisd xmm0, qword ptr [LJLint]
+	jae pbc3d4
+	xorpd xmm1, xmm1
+	ucomisd xmm0, xmm1
+	jb pbc3d4
+	jmp pbc3d5
+
+pbc3d4:									; need to correct
+	movsd xmm1, qword ptr [LJLint]
+	divsd xmm0, xmm1
+	cvtsd2si eax, xmm0					; should be floored during round
+	cvtsi2sd xmm0, eax
+	mulsd xmm0, qword ptr [LJLint]
+	movsd xmm1, qword ptr [esi + edi]
+	subsd xmm1, xmm0
+	movsd qword ptr [esi + edi], xmm1
+
+pbc3d5:
+	inc edx
+	cmp edx, 3
+	jne pbc3d3
+
+	add ebx, 20h
+	loop pbc3d2
+
+	; exit
+	movdqu xmm0, [ebp - 10h]
+	movdqu xmm1, [ebp - 20h]
+	mov ecx, dword ptr [ebp - 24h]
+	mov ebx, dword ptr [ebp - 28h]
+	mov esi, dword ptr [ebp - 2ch]
+	mov edx, dword ptr [ebp - 30h]
+	mov edi, dword ptr [ebp - 34h]
+	ldmxcsr dword ptr [ebp - 38h]		;restore mxcsr state
+	add esp, 40h
+	ret
+pbc3d endp
+
+
+dblBufferVec4Cpy proc buf1:DWORD, buf2:DWORD, nPart:DWORD
+	; enter
+	sub esp, 20h
+	movdqu [ebp - 10h], xmm0
+	mov dword ptr [ebp - 14h], ecx
+	mov dword ptr [ebp - 18h], esi
+	mov dword ptr [ebp - 1ch], edi
+	mov dword ptr [ebp - 20h], ebx
+
+	mov ecx, dword ptr [nPart]
+	mov esi, dword ptr [buf2]
+	mov edi, dword ptr [buf1]
+	xor ebx, ebx
+
+dblBufferVec4Cpy1:
+	movdqu xmm0, [esi + ebx]
+	movdqu [edi + ebx], xmm0
+	movdqu xmm0, [esi + ebx + 10h]
+	movdqu [edi + ebx + 10h], xmm0
+	add ebx, 20h
+	loop dblBufferVec4Cpy1
+
+	; exit
+	movdqu xmm0, [ebp - 10h]
+	mov ecx, dword ptr [ebp - 14h]
+	mov esi, dword ptr [ebp - 18h]
+	mov edi, dword ptr [ebp - 1ch]
+	mov ebx, dword ptr [ebp - 20h]
+	add esp, 20h
+	ret
+dblBufferVec4Cpy endp
+
+
+
+dblBufferVec4Mul proc buf:DWORD, vec:DWORD, nPart:DWORD
+	; enter
+	sub esp, 40h
+	movdqu [ebp - 10h], xmm0
+	movdqu [ebp - 20h], xmm1
+	movdqu [ebp - 30h], xmm2
+	mov dword ptr [ebp - 34h], esi
+	mov dword ptr [ebp - 38h], edi
+	mov dword ptr [ebp - 3ch], ecx
+	mov dword ptr [ebp - 40h], ebx
+
+	mov esi, dword ptr [vec]
+	movdqu xmm1, [esi]
+	movdqu xmm2, [esi + 10h]
+	mov edi, dword ptr [buf]
+	mov ecx, dword ptr [nPart]
+	xor ebx, ebx
+
+dblBufferVec4Mul1:
+	movdqu xmm0, [edi + ebx]
+	mulpd xmm0, xmm1
+	movdqu [edi + ebx], xmm0
+	movdqu xmm0, [edi + ebx + 10h]
+	mulpd xmm0, xmm2
+	movdqu [edi + ebx + 10h], xmm0
+	add ebx, 20h
+	loop dblBufferVec4Mul1
+
+	; exit
+	movdqu xmm0, [ebp - 10h]
+	movdqu xmm1, [ebp - 20h]
+	movdqu xmm2, [ebp - 30h]
+	mov esi, dword ptr [ebp - 34h]
+	mov edi, dword ptr [ebp - 38h]
+	mov ecx, dword ptr [ebp - 3ch]
+	mov ebx, dword ptr [ebp - 40h]
+	add esp, 40h
+	ret
+dblBufferVec4Mul endp
+
+
+
+dblBufferVec4Add proc buf:DWORD, vec:DWORD, nPart:DWORD
+	; enter
+	sub esp, 40h
+	movdqu [ebp - 10h], xmm0
+	movdqu [ebp - 20h], xmm1
+	movdqu [ebp - 30h], xmm2
+	mov dword ptr [ebp - 34h], esi
+	mov dword ptr [ebp - 38h], edi
+	mov dword ptr [ebp - 3ch], ecx
+	mov dword ptr [ebp - 40h], ebx
+
+	mov esi, dword ptr [vec]
+	movdqu xmm1, [esi]
+	movdqu xmm2, [esi + 10h]
+	mov edi, dword ptr [buf]
+	mov ecx, dword ptr [nPart]
+	xor ebx, ebx
+
+dblBufferVec4Add1:
+	movdqu xmm0, [edi + ebx]
+	addpd xmm0, xmm1
+	movdqu [edi + ebx], xmm0
+	movdqu xmm0, [edi + ebx + 10h]
+	addpd xmm0, xmm2
+	movdqu [edi + ebx + 10h], xmm0
+	add ebx, 20h
+	loop dblBufferVec4Add1
+
+	; exit
+	movdqu xmm0, [ebp - 10h]
+	movdqu xmm1, [ebp - 20h]
+	movdqu xmm2, [ebp - 30h]
+	mov esi, dword ptr [ebp - 34h]
+	mov edi, dword ptr [ebp - 38h]
+	mov ecx, dword ptr [ebp - 3ch]
+	mov ebx, dword ptr [ebp - 40h]
+	add esp, 40h
+	ret
+dblBufferVec4Add endp
+
+
+
+dblBufferVec4Sum proc buf:DWORD, nPart:DWORD, sum:DWORD
+	; enter
+	sub esp, 40h
+	movdqu [ebp - 10h], xmm0
+	movdqu [ebp - 20h], xmm1
+	movdqu [ebp - 30h], xmm2
+	mov dword ptr [ebp - 34h], ecx
+	mov dword ptr [ebp - 38h], ebx
+	mov dword ptr [ebp - 3ch], esi
+	mov dword ptr [ebp - 40h], edi
+
+	mov ecx, dword ptr [nPart]
+	mov esi, dword ptr [buf]
+	xorpd xmm0, xmm0
+	xorpd xmm1, xmm1
+	xor ebx, ebx
+
+dblBufferVec4Sum1:
+	movdqu xmm2, [esi + ebx]
+	addpd xmm0, xmm2
+	movdqu xmm2, [esi + ebx + 10h]
+	addpd xmm1, xmm2
+	add ebx, 20h
+	loop dblBufferVec4Sum1
+
+	mov edi, dword ptr [sum]
+	movdqu [edi], xmm0
+	movdqu [edi + 10h], xmm1
+
+	; exit
+	movdqu xmm0, [ebp - 10h]
+	movdqu xmm1, [ebp - 20h]
+	movdqu xmm2, [ebp - 30h]
+	mov ecx, dword ptr [ebp - 34h]
+	mov ebx, dword ptr [ebp - 38h]
+	mov esi, dword ptr [ebp - 3ch]
+	mov edi, dword ptr [ebp - 40h]
+	add esp, 40h
+	ret
+dblBufferVec4Sum endp
+
+
+
+dblBuffer2Vec4Sum proc buf1:DWORD, buf2:DWORD, nPart:DWORD, sum:DWORD
+	;enter
+	sub esp, 40h
+	movdqu [ebp - 10h], xmm0
+	movdqu [ebp - 20h], xmm1
+	mov dword ptr [ebp - 24h], esi
+	mov dword ptr [ebp - 28h], edx
+	mov dword ptr [ebp - 2ch], edi
+	mov dword ptr [ebp - 30h], ecx
+	mov dword ptr [ebp - 34h], ebx
+
+	mov esi, dword ptr [buf1]
+	mov edx, dword ptr [buf2]
+	mov edi, dword ptr [sum]
+	mov ecx, dword ptr [nPart]
+	xor ebx, ebx
+
+dblBuffer2Vec4Sum1:
+	movdqu xmm0, [esi + ebx]
+	movdqu xmm1, [edx + ebx]
+	addpd xmm0, xmm1
+	movdqu [edi + ebx], xmm0
+	movdqu xmm0, [esi + ebx + 10h]
+	movdqu xmm1, [edx + ebx + 10h]
+	addpd xmm0, xmm1
+	movdqu [edi + ebx + 10h], xmm0
+	add ebx, 20h
+	loop dblBuffer2Vec4Sum1
+
+	;exit
+	movdqu xmm0, [ebp - 10h]
+	movdqu xmm1, [ebp - 20h]
+	mov esi, dword ptr [ebp - 24h]
+	mov edx, dword ptr [ebp - 28h]
+	mov edi, dword ptr [ebp - 2ch]
+	mov ecx, dword ptr [ebp - 30h]
+	mov ebx, dword ptr [ebp - 34h]
+	add esp, 40h
+	ret
+dblBuffer2Vec4Sum endp
+
+
+dblBuffer2Vec3Dot proc buf1:DWORD, buf2:DWORD, nPart:DWORD, dot:DWORD
+	;enter
+	sub esp, 40h
+	movdqu [ebp - 10h], xmm0
+	movdqu [ebp - 20h], xmm1
+	mov dword ptr [ebp - 24h], esi
+	mov dword ptr [ebp - 28h], edx
+	mov dword ptr [ebp - 2ch], edi
+	mov dword ptr [ebp - 30h], ecx
+	mov dword ptr [ebp - 34h], ebx
+
+	mov esi, dword ptr [buf1]
+	mov edx, dword ptr [buf2]
+	mov edi, dword ptr [dot]
+	mov ecx, dword ptr [nPart]
+	xor ebx, ebx
+
+dblBuffer2Vec3Dot1:
+	movdqu xmm1, [esi + 4 * ebx]
+	movdqu xmm0, [edx + 4 * ebx]
+	mulpd xmm1, xmm0
+	movsd xmm0, xmm1
+	movhlps xmm1, xmm1
+	addsd xmm0, xmm1
+	movsd xmm1, qword ptr [esi + 4 * ebx + 10h]
+	mulsd xmm1, qword ptr [edx + 4 * ebx + 10h]
+	addsd xmm0, xmm1
+	movsd qword ptr [edi + ebx], xmm0
+	add ebx, 8
+	loop dblBuffer2Vec3Dot1
+
+	;exit
+	movdqu xmm0, [ebp - 10h]
+	movdqu xmm1, [ebp - 20h]
+	mov esi, dword ptr [ebp - 24h]
+	mov edx, dword ptr [ebp - 28h]
+	mov edi, dword ptr [ebp - 2ch]
+	mov ecx, dword ptr [ebp - 30h]
+	mov ebx, dword ptr [ebp - 34h]
+	add esp, 40h
+	ret
+dblBuffer2Vec3Dot endp
+
+
+
 pow proc base:DWORD, power:DWORD, result:DWORD
 	mov eax, power
 	fld qword ptr [eax]					;st0 = power
@@ -1399,6 +2235,7 @@ pow endp
 ; based on the famous "Quake" algorithm
 ; don't use this - it's not significantly faster than pow
 ; and is not as accurate
+comment @
 fastinvsqrt proc number:DWORD, result:DWORD
 	sub esp, 40h
 	movdqu [ebp - 10h], xmm0
@@ -1446,6 +2283,7 @@ fastinvsqrt proc number:DWORD, result:DWORD
 	add esp, 40h
 	ret
 fastinvsqrt endp
+@
 
 
 
--- asmMD.h
+++ asmMD.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define R_C 2.5
+
 extern "C" int _stdcall LibMain();
 extern "C" void _stdcall LJc(double* rij_, double* rc, double* Fij_);
 extern "C" void _stdcall LJuc(double* rij_, double* rc, double* U);
@@ -7,4 +9,13 @@ extern "C" void _stdcall LJinit(double* L);
 extern "C" void _stdcall LJforceEval(int nPart, double* X_, double* L, double* F_);
 extern "C" void _stdcall LJenergyEval(int nPart, double* X_, double* L, double* U);
 extern "C" void _stdcall LJenergyDiff(int nPart, double* X_, double* L, int i, double* deltaU);
+extern "C" void _stdcall LJpressureEval(int nPart, double* X_, double* L, double* Pxyz_);
+extern "C" void _stdcall LJstep(int nPart, double* X_, double* V_, double* F_, double* delT);
 extern "C" void _stdcall init3DGrid(int nPart, double* dens, double* L, double* grid);
+extern "C" void _stdcall pbc3d(int nPart, double* X_, double* L);
+extern "C" void _stdcall dblBufferVec4Cpy(double* buf1, double* buf2, int nPart);
+extern "C" void _stdcall dblBufferVec4Mul(double* buf, double* vec, int nPart);
+extern "C" void _stdcall dblBufferVec4Add(double* buf, double* vec, int nPart);
+extern "C" void _stdcall dblBufferVec4Sum(double* buf, int nPart, double* sum);
+extern "C" void _stdcall dblBuffer2Vec4Sum(double* buf1, double* buf2, int nPart, double* sum);
+extern "C" void _stdcall dblBuffer2Vec3Dot(double* buf1, double* buf2, int nPart, double* dot);
